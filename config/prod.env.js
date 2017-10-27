121867257e04598850a7393f260d24490a8ba9f2
lxf1002273683 <1002273683@qq.com>
2017-10-27 17:22:33 +0800

    区分正式环境打包和测试环境打包

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,11 @@
 // 发布环境 配置URL请求
+let url = '"http://apitest.ontheroadstore.com"'
+//  线上版本打包时，加 --prod参数
+if (process.argv[2] === 'prod') {
+	url = '"http://api.ontheroadstore.com"'
+}
+console.log(process.argv)
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://apitest.ontheroadstore.com"'
+  BASE_API: url
 }
