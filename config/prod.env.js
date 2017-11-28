c3d0910915a36d4b1f53c3201da9a3e4893d2760
lxf1002273683 <1002273683@qq.com>
2017-11-28 15:45:34 +0800

    配置文件更新

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,7 @@
 // 发布环境 配置URL请求
-let url = '"http://apitest.ontheroadstore.com"'
-//  线上版本打包时，加 --prod参数
-if (process.argv[2] === 'prod') {
-	url = '"http://api.ontheroadstore.com"'
-}
-console.log(process.argv)
+const heikuConfig = require('../api.config')
+
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: url
+  BASE_API: heikuConfig['url']
 }
